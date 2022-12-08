76aebae6b469442b168f791027e33338f1a3d8e2
Clara <clara_ljm@hotmail.com>
2022-12-08 20:56:32 -0300

    payment, one-to-one association

--- src/main/java/com/claramoreira/course/config/TestConfig.java
+++ src/main/java/com/claramoreira/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.claramoreira.course.entities.Category;
 import com.claramoreira.course.entities.Order;
 import com.claramoreira.course.entities.OrderItem;
+import com.claramoreira.course.entities.Payment;
 import com.claramoreira.course.entities.Product;
 import com.claramoreira.course.entities.User;
 import com.claramoreira.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 
+		orderRepository.save(o1);
+		
 	}
 }
